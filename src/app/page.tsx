efdb84ad9b1c29e5f28d80199d35da3d94ed4c82
ezequiaslopesdasilva <wyskzito@gmail.com>
2023-05-29 22:48:31 -0300

    feat: add visual API connection interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,113 @@
-import Image from 'next/image'
+"use client";
+
+import Button from "@/app/components/Button";
+import { MdOutlineContentCut, MdOutlineContentCopy } from "react-icons/md";
+import TextInput from "./components/Text";
+import Modal from "./components/Modal";
+import { useState } from "react";
+import { validateLongUrl } from "@/util/url";
+
+async function getShortUrl(longUrl: string) {
+  const response = await fetch(`/create?url=${longUrl}`);
+  const responseBody = (await response.json()) as {
+    newUrl: string;
+    error?: string;
+  };
+
+  return Object.assign(responseBody, {
+    statusCode: response.status,
+  });
+}
+
+interface GlobalErrorProps {
+  message?: string;
+  statusCode?: number;
+}
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [globalError, setGlobalError] = useState<GlobalErrorProps>();
+  const [shortUrl, setShortUrl] = useState<string>();
+  const [modalOpen, setModalOpen] = useState(false);
+  const [longUrl, setLongUrl] = useState<string>();
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  async function handleShortener() {
+    if (longUrl) {
+      try {
+        const cleanLongUrl = validateLongUrl(longUrl);
+        const { newUrl, error, statusCode } = await getShortUrl(cleanLongUrl);
+
+        if (error) {
+          setGlobalError({ message: error, statusCode: statusCode });
+          setModalOpen(true);
+        } else {
+          setShortUrl(newUrl);
+        }
+      } catch (error) {
+        let typedError = error as Error;
+        setGlobalError({ message: typedError.message, statusCode: 403 });
+        setModalOpen(true);
+      }
+    }
+  }
+
+  function copyToClipboard() {
+    if (shortUrl) {
+      navigator.clipboard.writeText(shortUrl);
+    }
+  }
+
+  return (
+    <div className="bg-gray-800 selection:bg-cyan-700 flex-wrap selection:text-cyan-50 w-screen h-screen flex justify-center items-center gap-10">
+      <div className="flex flex-col">
+        <span className="text-gray-600 text-2xl">Encurtador de Links</span>
+        <Modal
+          changeState={setModalOpen}
+          message={globalError?.message!}
+          statusCode={globalError?.statusCode!}
+          open={modalOpen}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center gap-2">
+          <TextInput
+            onChange={(event) => {
+              setLongUrl(event.target.value);
+              setGlobalError({});
+            }}
+            placeholder="Cole aqui seu URL"
+            className={`bg-gray-900 w-[320px] p-2 pl-4 pr-4 outline-none placeholder:text-gray-700 ${
+              globalError?.message && "border-red-400 text-red-400 border-2"
+            }`}
+          />
+          <Button
+            onClick={handleShortener}
+            className="bg-gray-900 group p-2 h-10 w-10 flex justify-center items-center"
+          >
+            <MdOutlineContentCut
+              size={20}
+              className="group-hover:fill-cyan-600 fill-gray-700"
+            />
+          </Button>
+        </div>
+        <div className="flex items-center gap-2">
+          <TextInput
+            disabled
+            value={shortUrl}
+            placeholder="URL Encurtado"
+            className="bg-gray-900 w-[320px] p-2 pl-4 pr-4 outline-none placeholder:text-gray-700"
+          />
+          <Button
+            disabled={!shortUrl}
+            onClick={copyToClipboard}
+            className="bg-gray-900 group p-2 h-10 w-10 flex justify-center items-center"
+          >
+            <MdOutlineContentCopy
+              size={20}
+              className={"fill-gray-700 group-hover:fill-cyan-600"}
+            />
+          </Button>
+        </div>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
