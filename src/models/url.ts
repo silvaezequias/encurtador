94ee8b00dfa76681927f6d0da92305b1173e3b9a
ezequiaslopesdasilva <wyskzito@gmail.com>
2023-05-29 22:46:33 -0300

    feat: add url representation template in database

---
+++ src/models/url.ts
@@ -0,0 +1,43 @@
+import ms from "ms";
+import database from "@/database";
+import { v4 as uuid } from "uuid";
+import { Url } from "@prisma/client";
+
+const URL_EXPIRES_AT = Date.now() + ms("1 days");
+
+async function create(data: Pick<Url, "longUrl" | "shortCode">) {
+  const urlObject = await database.url.create({
+    data: {
+      id: uuid(),
+      longUrl: data.longUrl,
+      shortCode: data.shortCode,
+      expiresAt: new Date(URL_EXPIRES_AT),
+    },
+  });
+
+  return urlObject;
+}
+
+async function findOneByShortCode(shortCode: string) {
+  const result = await database.url.findUnique({
+    where: { shortCode },
+  });
+
+  return result;
+}
+
+async function findOneByLongUrl(longUrl: string) {
+  const result = await database.url.findUnique({
+    where: { longUrl },
+  });
+
+  return result;
+}
+
+const url = {
+  create,
+  findOneByLongUrl,
+  findOneByShortCode,
+};
+
+export default url;
